514b3ced68f8479211bd78738398e5da24615ddf
Foxydemon88 <122745278+Foxydemon88@users.noreply.github.com>
2026-07-15 19:57:51 -0500

    Update CameraStabilizerPatches.cs

--- CameraStabilizerPatches.cs
+++ CameraStabilizerPatches.cs
@@ -5,45 +5,44 @@ using HarmonyLib;
 
 namespace FlatscreenATTMod
 {
-    [HarmonyPatch]
-    internal static class CameraStabilizerPatches
-    {
-        private static IEnumerable<MethodBase> TargetMethods()
-        {
-            var type = FindType("CameraStabelizer") ?? FindType("CameraStabilizer");
-            if (type == null)
-            {
-                yield break;
-            }
+	[HarmonyPatch]
+	internal static class CameraStabilizerPatches
+	{
+		private static IEnumerable<MethodBase> TargetMethods()
+		{
+			Type type = FindType("CameraStabelizer") ?? FindType("CameraStabilizer");
+			if (type == null)
+			{
+				yield break;
+			}
+			string[] methodNames = new string[4] { "StabelizeCamera", "StabilizeCamera", "LateUpdate", "Update" };
+			for (int i = 0; i < methodNames.Length; i++)
+			{
+				MethodInfo method = type.GetMethod(methodNames[i], BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+				if (method != null)
+				{
+					yield return method;
+				}
+			}
+		}
 
-            var methodNames = new[] { "StabelizeCamera", "StabilizeCamera", "LateUpdate", "Update" };
-            for (var i = 0; i < methodNames.Length; i++)
-            {
-                var method = type.GetMethod(methodNames[i], BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                if (method != null)
-                {
-                    yield return method;
-                }
-            }
-        }
+		private static bool Prefix()
+		{
+			return !FlatscreenMod.SuppressOpenXRInputUpdates;
+		}
 
-        private static bool Prefix()
-        {
-            return !FlatscreenMod.SuppressOpenXRInputUpdates;
-        }
-
-        private static Type FindType(string typeName)
-        {
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                var type = assembly.GetType(typeName);
-                if (type != null)
-                {
-                    return type;
-                }
-            }
-
-            return null;
-        }
-    }
+		private static Type FindType(string typeName)
+		{
+			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			foreach (Assembly assembly in assemblies)
+			{
+				Type type = assembly.GetType(typeName);
+				if (type != null)
+				{
+					return type;
+				}
+			}
+			return null;
+		}
+	}
 }
